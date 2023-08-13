994125ced84ab23656bae33338eef5826f8f0aae
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-08-13 09:17:40 +0700

    auth

---
+++ pages/login.js
@@ -0,0 +1,29 @@
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+function login() {
+  const [calledPush, setCalledPush] = useState(false); // handle error
+  const {data: session} = useSession();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if(session){
+        if (calledPush) { //handle error
+            return;
+        }
+        router.replace('/');
+        setCalledPush(true); // handle error
+    }
+  }, [session])
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <button className='bg-blue-400 p-2 rounded-xl px-3 text-white' onClick={() => signIn()}>
+            Login with google
+        </button>
+    </div>
+  )
+}
+
+export default login
