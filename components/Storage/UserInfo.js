17e947a765b4b41a4bbc98710c70805f113495d3
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-08-14 23:56:58 +0700

    perbaikan

--- components/Storage/UserInfo.js
+++ components/Storage/UserInfo.js
@@ -30,7 +30,7 @@ function UserInfo() {
             </div>
             <div className='bg-blue-200 p-2 rounded-lg cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-                strokeWidth={1.5} onClick={()=> handleSignOut}
+                strokeWidth={1.5} onClick={()=> handleSignOut()}
                 stroke="currentColor" 
                 className="w-6 h-6 text-blue-500
                 hover:animate-pulse transition-all ">
